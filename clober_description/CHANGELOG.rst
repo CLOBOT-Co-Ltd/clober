5ab9617c7755e55079851b89cd411ff59202791d
ClobotLucy <ros@clobot.co.kr>
2021-05-10 19:01:40 +0900

    add description

---
+++ clober_description/CHANGELOG.rst
@@ -0,0 +1,9 @@
+
+Changelog for clober_description pacakage
+^^^^^^^^^^^^^^^^^^^^^^^
+
+
+
+0.1.0 (2021-05-10)
+------------------
+* add initial clober_description pacakage
---
+++ clober_description/launch/description_launch.py
@@ -0,0 +1,50 @@
+# Copyright 2021 clobot
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import DeclareLaunchArgument
+import xacro
+
+def generate_launch_description():
+
+    dir = get_package_share_directory('clober_description')
+    model_file = os.path.join(dir,'urdf','clober.urdf.xacro')
+    rviz_file = os.path.join(dir,'rviz','clober.rviz')
+
+    return LaunchDescription([
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            arguments=[model_file]
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_file],
+            # output='log'
+            ),
+
+    ])
