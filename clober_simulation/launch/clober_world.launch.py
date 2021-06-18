a9b3794ff8579c820f55dadf000569cfa7663ec4
ClobotLucy <ros@clobot.co.kr>
2021-06-18 10:25:25 +0900

    change gazebo package name

--- clober_simulation/launch/clober_world.launch.py
+++ clober_simulation/launch/clober_world.launch.py
@@ -8,16 +8,17 @@ from launch.actions import IncludeLaunchDescription
 from launch.actions import ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+from launch.substitutions import EnvironmentVariable
 
 def generate_launch_description():
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     world_file_name = 'clober.model'
-    world = os.path.join(get_package_share_directory('clober_gazebo'), 'worlds', world_file_name)
+    world = os.path.join(get_package_share_directory('clober_simulation'), 'worlds', world_file_name)
 
     launch_file_dir = os.path.join(get_package_share_directory('clober_description'), 'launch')
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
-    gazebo_model_path = os.path.join(get_package_share_directory('clober_gazebo'), 'models')
+    gazebo_model_path = os.path.join(get_package_share_directory('clober_simulation'), 'models')
 
 
     return LaunchDescription([
